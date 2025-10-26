584b2b47e4e82337bf62cc4d2ef652714eda069c
MaryamCDM <maryamcadetmarthe10@gmail.com>
2025-10-26 17:11:47 +0100

    Delete Exercices/azertyu.sh

--- Exercices/azertyu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-echo "2016 : $(grep -h "Location" "../Exercice1/ann/2016"/*.ann | wc -l)"
-echo "2017 : $(grep -h "Location" "../Exercice1/ann/2017"/*.ann | wc -l)"
-echo "2018 : $(grep -h "Location" "../Exercice1/ann/2018"/*.ann | wc -l)"
-chmod +x
